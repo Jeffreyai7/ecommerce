94893471326b9852bbd43b1e7650950be3e11cd1
jeffreyai7 <jirukeh@gmail.com>
2024-05-20 01:12:03 +0100

    edited plugins

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  // css: {
-  //   modules: {
-  //     localsConvention: "camelCase",
-  //   },
-  // },
+  plugins: [react({ jsxRuntime: 'classic' })],
+  
 })
+
